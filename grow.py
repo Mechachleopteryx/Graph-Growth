f2850c71944088c6dc54208da52847680743d3bb
mb3152 <Maxwell@dhcp-128-32-52-230.lips.berkeley.edu>
2013-11-18 11:38:36 -0800

    added pybayes to main

--- grow.py
+++ grow.py
@@ -25,12 +25,12 @@ import pybayes
 # 3. Same thing, but this time fixing the number of incoming links and randomly allocating their origins. Likewise, but preserves in-degree.
 
 # get bnt stuff working better. 
-# build setup.py
 
 
-#this does the whole shebang! 
+
+#this does the whole shebang!
 def grow(csvfile, getgraph = True, drawspectral = True, force_connected = True, usenx= True, sparse = True, plot = False, directed = True, randomgrowth=False, wholegrowth=False,growthfactor=100, num_measurements = 10, verbose = True, plotx = 'nodegrowth', ploty = 'maxclique', ploty2 = 'modularity',drawgraph = 'triangulated', draw= True):
-	#load(csvfile = csvfile, verbose = verbose)
+	load(csvfile = csvfile, verbose = verbose)
 	x = growgraph(usenx= usenx, getgraph = getgraph, drawspectral = drawspectral, force_connected = force_connected, sparse = sparse, plot = plot, directed = directed, randomgrowth= randomgrowth, wholegrowth=wholegrowth,growthfactor=growthfactor, num_measurements = num_measurements, verbose = verbose, plotx = plotx, ploty = ploty, ploty2 = ploty2, drawgraph = drawgraph, draw= draw)
 	return x
 
@@ -341,7 +341,158 @@ def growgraph(usenx= True, force_connected = True, sparse = True, plot = False, 
 		return(df)
 
 
+def markov_blanket(G, n):
+    """
+    Returns the Markov blanket of `n`.
 
+    The Markov blanket consists of the parents of `n`, the children of `n`, and
+    any other parents of the children of `n`.
+
+    Parameters
+    ----------
+    G : DiGraph
+        A direct acyclic graph.
+    n : node
+        A node in `G`.
+
+    Returns
+    -------
+    blanket : set
+        The Markov blanket of `n`.
+
+    Notes
+    -----
+    The procedure works for any directed graph, but the interpretation of the
+    result is valid only when `G` is a DAG.
+
+    """
+    blanket = set(G.pred[n])
+    children = list(G.succ[n].keys())
+    blanket.update(children)
+    for child in children:
+        blanket.update(G.pred[child])
+    return blanket
+
+def moral_graph(G):
+    """
+    Returns the moral graph of `G`.
+
+    The moral graph is an undirected graph where every node in `G` is connected
+    to its Markov blanket.
+
+    Parameters
+    ----------
+    G : DiGraph
+        A direct acyclic graph.
+
+    Returns
+    -------
+    MG : Graph
+        The moral graph of `G`.
+
+    """
+    MG = nx.Graph()
+    for u in G:
+        blanket = markov_blanket(G, u)
+        MG.add_edges_from([(u, v) for v in blanket if v != u])
+    return MG
+
+def maximum_spanning_tree(G, weight='weight'):
+    """
+    Return a maximum spanning tree or forest of an undirected, weighted graph.
+
+    Parameters
+    ----------
+    G : Graph
+        An undirected, weighted graph.
+    weight : str
+        The attribute used as weights.
+
+    Returns
+    -------
+    T : Graph
+       A minimum spanning tree or forest.
+
+    Notes
+    -----
+    The maximum spanning tree is unique only if every weight is unique.
+
+    See Also
+    --------
+    nx.minimum_spanning_tree
+
+    """
+    # Leave the original graph untouched.
+    G = G.copy()
+
+    # Negate all the weights.
+    for u, v, data in G.edges_iter(data=True):
+        data[weight] = -data.get(weight, 1)
+
+    # Build the minimum spanning tree.
+    T = nx.minimum_spanning_tree(G, weight)
+
+    # Un-negate all the weights.
+    for u, v, data in T.edges_iter(data=True):
+        data[weight] = -data.get(weight, 1)
+
+    return T
+
+### This is networkx.algorithms.clique.make_max_clique_graph but modified
+### to include weights according to the intersection of the cliques.
+def clique_graph(G, create_using=None, name=None):
+    """Create the maximal clique graph of a graph.
+
+    Finds the maximal cliques and treats these as nodes.
+    The nodes are connected if they have common members in
+    the original graph.  Theory has done a lot with clique
+    graphs, but I haven't seen much on maximal clique graphs.
+
+    Notes
+    -----
+    This should be the same as make_clique_bipartite followed
+    by project_up, but it saves all the intermediate steps.
+    """
+    cliq = list(map(set, nx.find_cliques(G)))
+    if create_using:
+        B = create_using
+        B.clear()
+    else:
+        B = nx.Graph()
+    if name is not None:
+        B.name = name
+
+    to_node = lambda cl: tuple(sorted(cl))
+    for i, cl in enumerate(cliq):
+        u = to_node(cl)
+        B.add_node(u)
+        for j, other_cl in enumerate(cliq[:i]):
+            intersect = cl & other_cl
+            if intersect:     # Not empty
+                B.add_edge(u, to_node(other_cl), weight=len(intersect))
+    return B
+
+def junction_tree(G):
+    """Return a junction tree of `G`.
+
+    A junction tree of `G` is a maximal weight spanning tree of the clique
+    graph of `G`.  Its width is the size of the largest clique of `G` minus
+    one, and is minimal.
+
+    Parameters
+    ----------
+    G : DiGraph
+        A direct acyclic graph.
+
+    Returns
+    -------
+    J : Graph
+        A junction tree.
+
+    """
+    CG = clique_graph(G)
+    J = maximum_spanning_tree(CG)
+    return J
 
 
 
